f8ad46d3617218f310d5e444653897beb521fe2a
Nguyễn Anh Tú <129037773+elio-perlman-13@users.noreply.github.com>
2025-12-06 05:57:10 +0000

    increase searching attempt

--- tabubu.cpp
+++ tabubu.cpp
@@ -5769,8 +5769,8 @@ int main(int argc, char* argv[]) {
             CFG_MAX_ITER_PER_SEGMENT = min(CFG_MAX_ITER_PER_SEGMENT, 200);
             CFG_MAX_NO_IMPROVE = min(CFG_MAX_NO_IMPROVE, 50);
             CFG_KNN_K = min(CFG_KNN_K, int(n)); // modest k for small n
-        } else if (n <= 100) {
-            CFG_NUM_INITIAL = min(CFG_NUM_INITIAL, 1);
+        } else if (n <= 200) {
+            CFG_NUM_INITIAL = min(CFG_NUM_INITIAL, 5);
             CFG_MAX_SEGMENT = min(CFG_MAX_SEGMENT, 50);
             CFG_MAX_ITER_PER_SEGMENT = min(CFG_MAX_ITER_PER_SEGMENT, 200);
             CFG_MAX_NO_IMPROVE = min(CFG_MAX_NO_IMPROVE, 50);
