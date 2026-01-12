df570168f5cf13848545040a2d3a92cdba08d4f9
Nguyễn Anh Tú <129037773+elio-perlman-13@users.noreply.github.com>
2026-01-12 14:56:54 +0000

    mix mode

--- tabubu.cpp
+++ tabubu.cpp
@@ -5780,7 +5780,7 @@ Solution tabu_search(const Solution& initial_solution, int num_initial_sol) {
                 best_solution_score_now = neighbor_score;
                 //updated_edge_records(neighbor);
                 //Solution tmp = updated_elite_set(neighbor);
-                scoring_mode_segment = 1;
+                scoring_mode_segment = 0;
             } else if (neighbor_score + 1e-12 < current_score ||
                        (std::abs(neighbor_score - current_score) <= 1e-12 &&
                         neighbor.total_makespan + 1e-12 < current_cost)) {
@@ -5804,13 +5804,13 @@ Solution tabu_search(const Solution& initial_solution, int num_initial_sol) {
                 best_feasible_makespan = current_sol.total_makespan;
                 best_feasible_solution = current_sol;
                 best_cost = best_feasible_makespan;
-                scoring_mode_segment = 1;
+                scoring_mode_segment = 0;
             } else if (neighbor_feasible &&
                        neighbor.total_makespan + 1e-12 < best_feasible_makespan) {
                 best_feasible_makespan = neighbor.total_makespan;
                 best_feasible_solution = neighbor;
                 best_cost = best_feasible_makespan;
-                scoring_mode_segment = 1;
+                scoring_mode_segment = 0;
             }
             if (neighbor_score + 1e-12 < best_segment_score ||
                 (std::abs(neighbor_score - best_segment_score) <= 1e-12 &&
@@ -5828,8 +5828,8 @@ Solution tabu_search(const Solution& initial_solution, int num_initial_sol) {
             iter++;
             if (no_improve_iters >= CFG_MAX_NO_IMPROVE || iter >= CFG_MAX_ITER_PER_SEGMENT){
                 if (no_improve_iters >= CFG_MAX_NO_IMPROVE) {
-                    //if (scoring_mode_iter == 1) scoring_mode_segment = 0;
-                    //else if (scoring_mode_iter == 0) scoring_mode_segment = 1;
+                    if (scoring_mode_iter == 1) scoring_mode_segment = 0;
+                    else if (scoring_mode_iter == 0) scoring_mode_segment = 1;
                     cout << "No improvement in " << CFG_MAX_NO_IMPROVE << " iters, switching scoring mode to "
                          << scoring_mode_segment << "\n";
                 }
