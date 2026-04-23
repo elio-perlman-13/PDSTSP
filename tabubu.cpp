bd2d3387ff45e2bd6a37d021b1c4c56d9fcc7256
Nguyễn Anh Tú <129037773+elio-perlman-13@users.noreply.github.com>
2026-04-23 15:17:46 +0000

    table 3: base

--- tabubu.cpp
+++ tabubu.cpp
@@ -5989,7 +5989,6 @@ Solution tabu_search(const Solution& initial_solution, int num_initial_sol,  vec
                 current_cost = neighbor.total_makespan;
                 current_score = neighbor_score;
             } */
-            
             score[selected_neighbor] += gamma3;
             no_improve_iters++;
         }
